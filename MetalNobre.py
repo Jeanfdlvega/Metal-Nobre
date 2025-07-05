d4e4dce9a866770a633531e0268c097579c366ff
Jeanfdlvega <137357130+Jeanfdlvega@users.noreply.github.com>
2025-07-05 16:17:45 -0300

    Add files via upload

---
+++ MetalNobre.py
@@ -0,0 +1,31 @@
+altura = int(input("Digite a altura: "))
+largura = int(input("Digite a largura: "))
+social = int(input("Digite a largura social: "))
+
+
+metalon_folha = int(input("Digite a medida do metalon que vai ficar virado para face do portão: "))
+caixaria = int(input("Digite a medida da caixaria: "))
+folga_caixaria = int(input("Digite a medida da folga de caixaria: "))
+folga_social = int(input("Digite a medida da folga do social: "))
+metalon_social = int(input("Digite a medida do metalon que vai ficar virado para face do social: "))
+vao_livre_cima = int(input("Digite a medida do vão livre da cima: "))
+vao_livre_baixo = int(input("Digite a medida do vão livre de baixo: "))
+
+  # em m² se cm for usado
+
+print("\n=== PORTÃO BASCULANTE ===\n")
+print(f"Altura: {altura} mm")
+print(f"Largura: {largura} mm")
+print(f"Largura do Social: {social} mm")
+print(f"Metalon da folha (frente do portão): {metalon_folha} mm")
+print(f"Folga da Caixaria: {folga_caixaria} mm")
+print(f"Folga do Social: {folga_social} mm")
+print(f"Metalon do Social (frente do social): {metalon_social} mm")
+print(f"Vão Livre Superior: {vao_livre_cima} mm")
+print(f"Vão Livre Inferior: {vao_livre_baixo} mm")
+
+
+
+# Cálculo dos lambril do portão
+
+
